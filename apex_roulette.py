abda2fdb612410fa76fb38f5cdf84c6ba0bc42a9
taichaemon <taichaemon.eng@gmail.com>
2025-02-03 00:33:21 +0900

    Update apex_roulette.py

--- apex_roulette.py
+++ apex_roulette.py
@@ -3,55 +3,6 @@ import streamlit as st
 import random as rd
 import streamlit.components.v1 as stc
 
-HIDE_ST_STYLE = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                #MainMenu {
-                visibility: hidden;
-                height: 0%;
-                }
-                header {
-                visibility: hidden;
-                height: 0%;
-                }
-                footer {
-                visibility: hidden;
-                height: 0%;
-                }
-				        .appview-container .main .block-container{
-                            padding-top: 1rem;
-                            padding-right: 3rem;
-                            padding-left: 3rem;
-                            padding-bottom: 1rem;
-                        }  
-                        .reportview-container {
-                            padding-top: 0rem;
-                            padding-right: 3rem;
-                            padding-left: 3rem;
-                            padding-bottom: 0rem;
-                        }
-                        header[data-testid="stHeader"] {
-                            z-index: -1;
-                        }
-                        div[data-testid="stToolbar"] {
-                        z-index: 100;
-                        }
-                        div[data-testid="stDecoration"] {
-                        z-index: 100;
-                        }
-                </style>
-"""
-
-st.markdown(HIDE_ST_STYLE, unsafe_allow_html=True)
 st.title('レジェンドルーレットアプリ')
 st.caption('created by taichaemon')
 st.subheader('APEX全レジェンド26人')
